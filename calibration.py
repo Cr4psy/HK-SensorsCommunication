235693636fe0101315438504775e64b27d1a5211
sofianh94 <sofianh@kth.se>
2017-09-20 11:27:36 +0200

    calibration function implemented and radar serial communication

--- calibration.py
+++ calibration.py
@@ -15,9 +15,14 @@ import math
 #adc = Adafruit_ADS1x15.ADS1115()
 
 # Or create an ADS1015 ADC (12-bit) instance.
-adc = Adafruit_ADS1x15.ADS1015()
-
-# Note you can change the I2C address from its default (0x48), and/or the I2C
+adc = Adafruit_ADS1x15.ADS1015() 
+minDis = [20,100,0,0] 
+maxDis = [150,500,999,999] 
+minVolt = [-2,1.4,-2,-2] 
+average_0 = 0 
+average_1 = 0 
+iter = 20
+# Note  you can change the I2C address from its default (0x48), and/or the I2C
 # bus by passing in these optional parameters:
 #adc = Adafruit_ADS1x15.ADS1015(address=0x49, busnum=1)
 
@@ -38,26 +43,53 @@ print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
 print('-' * 37)
 # Main loop.
 while True:
-    # Read all the ADC channel values in a list.
-    values = [0]*4
-    for i in range(4):
-        # Read the specified ADC channel using the previously set gain value.
-        values[i] = adc.read_adc(i, gain=GAIN)*4.096/2047
-        # Note you can also pass in an optional data_rate parameter that controls
-        # the ADC conversion time (in samples/second). Each chip has a different
-        # set of allowed data rate values, see datasheet Table 9 config register
-        # DR bit values.
-        #values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
-        # Each value will be a 12 or 16 bit signed integer value depending on the
-        # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
-    # Print the ADC values.
-    #if (values[0] > 2.7):
-    #            values[0] = 10
-    #elif (values[0] < 0.4):
-    #            values[0] = 999
-    #else:
-    #            values[0] = 57.6/(values[0]-0.08)
-		#values[0] = 0.0002*math.pow(values[0],3)-0.0366*math.pow(values[0],2)+2.4507*values[0]-19.674
+    average_0 = 0
+    average_1 = 0
+    # Filtering loop (average of 10 samples)
+    for f in range(iter):
+    	# Read all the ADC channel values in a list.
+    	values = [0]*4
+    	for i in range(4):
+        	# Read the specified ADC channel using the previously set gain value.
+        	values[i] = adc.read_adc(i, gain=GAIN)*4.096/2047
+		while (values[i]<minVolt[i]):
+			time.sleep(0.04)
+			values[i] = adc.read_adc(i, gain=GAIN)*4.096/2047
+			
+		# Note you can also pass in an optional data_rate parameter that controls
+        	# the ADC conversion time (in samples/second). Each chip has a different
+        	# set of allowed data rate values, see datasheet Table 9 config register
+        	# DR bit values.
+        	#values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
+        	# Each value will be a 12 or 16 bit signed integer value depending on the
+        	# ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
+    	# Print the ADC values.
+    	#if (values[0] > 3):
+        #        values[0] = 10
+    	#elif (values[0] < 0.4):
+        #        values[0] = 999
+    	#else:
+        values[0] = (1/values[0]+0.0073)/0.017
+
+    	#if (values[1] > 3):
+	#	values[1] = 10
+    	#elif (values[1] < 1.4):
+	#	values[1] = 999
+    	#else:
+	values[1] = math.exp((1/values[1]+0.4776)/0.193)
+	average_0 = average_0 + values[0]
+	average_1 = average_1 + values[1]
+	time.sleep(0.04)
+    values[0] = average_0/iter
+    values[1] = average_1/iter
+
+    for i in range(len(values)):
+	if (values[i] > maxDis[i]):
+		values[i]=999
+	elif (values[i] < minDis[i]):
+		values[i]=0
+
+
     print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
     # Pause for half a second.
-    time.sleep(0.5)
+    time.sleep(0.05)
